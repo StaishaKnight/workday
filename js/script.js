dc39a02cb2163fbea68dcfc86c6e87c420c8ccd4
StaishaKnight <staishaknight@gmail.com>
2021-12-08 14:17:03 -0600

    added vlaues to local storage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var date= $("#currentDay").text(moment().format('MMMM Do YYYY, h:m a'))
+var date= $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'))
 
-//create save button that reads the task description tect area and places it into local storage.setitem
+//create save button that reads the task description text area and places it into local storage.setitem
 $(".savebutton").on("click",function(){
 var descriptionvalue= $(this).siblings(".taskDescription").val()
 var timekey= $(this).parent().attr("id")
@@ -10,5 +10,15 @@ localStorage.setItem(timekey,descriptionvalue)
 
 })
 //Grab the values of anything of saved to local storage.getitem and have them render in their appropriate text areas based in local id
+$("#9 .taskDescription").val(localStorage.getItem("9"))
+$("#10 .taskDescription").val(localStorage.getItem("10"))
+$("#11 .taskDescription").val(localStorage.getItem("11"))
+$("#12 .taskDescription").val(localStorage.getItem("12"))
+$("#13 .taskDescription").val(localStorage.getItem("13"))
+$("#14 .taskDescription").val(localStorage.getItem("14"))
+$("#15 .taskDescription").val(localStorage.getItem("15"))
+$("#16 .taskDescription").val(localStorage.getItem("16"))
+$("#17 .taskDescription").val(localStorage.getItem("17"))
+
 
 //create function to conditionally render the correct colors to the time blocks based on if they are in past present and future
